9b3fed337e291985b791649032e6853dba48135b
humbertoguerrer <humberto.guerrer@gmail.com>
2021-01-11 21:06:06 -0300

    Projeto criado

---
+++ src/main/java/com/hgn/loteriazup/LoteriazupApplication.java
@@ -0,0 +1,13 @@
+package com.hgn.loteriazup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoteriazupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoteriazupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hgn/loteriazup/LoteriazupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgn.loteriazup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoteriazupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
