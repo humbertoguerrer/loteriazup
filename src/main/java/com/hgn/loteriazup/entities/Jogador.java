1cb5b68d59378c1b059ab6a6b1a34a238e211d0d
humbertoguerrer <humberto.guerrer@gmail.com>
2021-01-13 21:59:09 -0300

    Configurada conexão com MYSQL

--- src/main/java/com/hgn/loteriazup/entities/Jogador.java
+++ src/main/java/com/hgn/loteriazup/entities/Jogador.java
@@ -19,7 +19,7 @@ import com.fasterxml.jackson.annotation.JsonProperty.Access;
 public class Jogador implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	@JsonIgnore
+	
 	private String nome;
 
 	@Id
