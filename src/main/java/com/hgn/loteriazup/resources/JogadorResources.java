a7759795e7640b9eafd2277fd287632a3e599250
humbertoguerrer <humberto.guerrer@gmail.com>
2021-01-14 19:41:15 -0300

    Delegada a lógica de instanciação para a classe service

--- src/main/java/com/hgn/loteriazup/resources/JogadorResources.java
+++ src/main/java/com/hgn/loteriazup/resources/JogadorResources.java
@@ -1,14 +1,9 @@
 package com.hgn.loteriazup.resources;
 
-import java.util.LinkedHashSet;
-import java.util.Random;
-import java.util.Set;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hgn.loteriazup.entities.Jogador;
+import com.hgn.loteriazup.repositories.JogadorRepository;
 import com.hgn.loteriazup.services.JogadorService;
 
 @RestController
@@ -27,24 +23,19 @@ public class JogadorResources {
 	@Autowired
 	private JogadorService service;
 
-	@RequestMapping(value = "/{email}", method = RequestMethod.GET)
-	public ResponseEntity<Jogador> find(@PathVariable String email) {
-		Jogador jogador = service.findByEmail(email);
-		return ResponseEntity.ok().body(jogador);
+	@Autowired
+	private JogadorRepository repository;
 
+	@RequestMapping(value = "/{email}", method = RequestMethod.GET)
+	public Jogador find(@PathVariable String email) {
+		Jogador jogador = repository.findByEmail(email);
+		return jogador;
 	}
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Jogador add(@Valid @RequestBody Jogador jogador) {
-		jogador.setNome(jogador.getNome());
-		jogador.setEmail(jogador.getEmail());
-		Set<Integer> apostas = new LinkedHashSet<>();
-		for (int i = 1; i <= jogador.getQtdAposta(); i++) {
-			Integer aposta = new Random().nextInt(1000);
-			apostas.add(aposta);
-		}
-		jogador.setApostas(apostas);
 		return service.save(jogador);
+
 	}
 }
--- src/main/java/com/hgn/loteriazup/services/JogadorService.java
+++ src/main/java/com/hgn/loteriazup/services/JogadorService.java
@@ -1,5 +1,9 @@
 package com.hgn.loteriazup.services;
 
+import java.util.LinkedHashSet;
+import java.util.Random;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,17 +17,20 @@ public class JogadorService {
 	@Autowired
 	private JogadorRepository repository;
 
-	public Jogador findByEmail(String email) {
-		return repository.findByEmail(email);
-	}
-
 	public Jogador save(Jogador jogador) {
 		Jogador jogadorExistente = repository.findByEmail(jogador.getEmail());
 		if (jogadorExistente != null && jogadorExistente.getEmail().equals(jogador.getEmail())) {
 			throw new ServicesExceptions("Já existe um jogador cadastrado com este e-mail.");
+		} else {
+			Set<Integer> apostas = new LinkedHashSet<>();
+			for (int i = 0; i <= jogador.getQtdAposta(); i++) {
+				Integer aposta = new Random().nextInt(1000);
+				apostas.add(aposta);
+			}
+
+			jogador.setApostas(apostas);
+			return repository.save(jogador);
+
 		}
-
-		return repository.save(jogador);
-
 	}
 }
